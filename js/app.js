c723ac96bd9bfb8c996aae8f1d80eef49d95b58c
rtruneh <rosa.truneh@gmail.com>
2021-04-12 20:10:51 -0500

    added questions and points

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 // 'use strict'
 // //asking user if they want to play
-// let answer1 = confirm('Would you like to play a game?')
-// if (!answer1) {
-//   alert('sorry to see you go');
-// } else {
-//   alert('come on in!');
-// }
+let answer1 = confirm('Would you like to play a game?')
+if (!answer1) {
+  alert('sorry to see you go');
+} else {
+  alert('come on in!');
+}
 
-// //once user says ok, ask for name then enter the page
-// let user = prompt('what is your name?');
-// while (!user) {
-//   alert ('Seriously, I need your name.');
-//   user = prompt('What is your name?');
-// } 
-// if (user.length > 0) {
-// confirm('Hi ' + user + ' let\'s play!');
-// }
+//once user says ok, ask for name then enter the page
+let user = prompt('what is your name?');
+while (!user) {
+  alert ('Seriously, I need your name.');
+  user = prompt('What is your name?');
+} 
+if (user.length > 0) {
+confirm('Hi ' + user + ' let\'s play!');
+}
 
 //quiz
 let points = 0
@@ -70,29 +70,10 @@ let question5 = prompt('Is the author a home owner?', 'enter yes or no');
     points--;
     alert('Total points: ' + points);
 }
+
 }
 
 
-// convert the variable question1 to lower case to evaluate it
-// switch (question1.toLowerCase()) {
-//   case 'yes': 
-//   break;
-//   case 'YES':
 
- 
-// //break statement follows each case to 'break' the loop not needed if return is present
-//   break;
-//   case 'blue':
-//   alert('your favorite color was blue!');
-//   break;
-// // default statement is what is fired in there event none of the cases occur
-//   default:
-//   alert('I don\'t know what your favorite color was ¯\\_(ツ)_/¯');
-// }
 
-//! in front of the word is false
-// the below will run until user enters information
 
-//display points and give points to user
-
-// alert('you have ' + userPoints + ' points')
