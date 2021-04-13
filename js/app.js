427232d777808aa9692b47e1dff0fb5afff43358
rtruneh <rosa.truneh@gmail.com>
2021-04-12 19:56:50 -0500

    added questions with point function

--- js/app.js
+++ js/app.js
@@ -1,50 +1,77 @@
-'use strict'
-//asking user if they want to play
-let answer1 = confirm('Would you like to play a game?')
-if (!answer1) {
-  alert('sorry to see you go');
-} else {
-  alert('come on in!');
-}
+// 'use strict'
+// //asking user if they want to play
+// let answer1 = confirm('Would you like to play a game?')
+// if (!answer1) {
+//   alert('sorry to see you go');
+// } else {
+//   alert('come on in!');
+// }
 
-//once user says ok, ask for name then enter the page
-let user = prompt('what is your name?');
-while (!user) {
-  alert ('Seriously, I need your name.');
-  user = prompt('What is your name?');
-} 
-if (user.length > 0) {
-confirm('Hi ' + user + ' let\'s play!');
-}
+// //once user says ok, ask for name then enter the page
+// let user = prompt('what is your name?');
+// while (!user) {
+//   alert ('Seriously, I need your name.');
+//   user = prompt('What is your name?');
+// } 
+// if (user.length > 0) {
+// confirm('Hi ' + user + ' let\'s play!');
+// }
 
 //quiz
+let points = 0
 function myQuiz() {
   let question1 = prompt('Does the author have a child?', 'enter yes or no');
- if (question1 === 'yes'.toUpperCase)
-
-  let question2 = prompt('Did the author work at the airport?', 'enter yes or no');
-  while (question2 === 'no') {
-    alert ('try again');
-    question2 = prompt('Did the author work at the airport?', 'enter yes or no');
-  } 
-
-  let question3 = prompt('Did the author attend TSU?', 'enter yes or no');
-  while (question3 === 'yes') {
-    alert ('try again');
-    question3 = prompt('Did the author work at the airport?', 'enter yes or no');
-  } 
-  
-
-  let question4 = prompt('Is the author a man?', 'enter yes or no');
-
-  let question5 = prompt('Is the author a home owner?', 'enter yes or no');
-
+    if (question1.toLowerCase() === 'yes') {
+  alert('That is correct');
+  points++;
+  alert('Total points: ' + points)
+ } else {
+   alert('Incorrect');
+   points--;
+   alert('Total points: ' + points);
+ }
+ let question2 = prompt('Did the author work at the airport?', 'enter yes or no');
+    if (question2.toLowerCase() === 'yes') {
+  alert('That is correct');
+  points++;
+  alert('Total points: ' + points)
+ } else {
+   alert('Incorrect');
+   points--;
+   alert('Total points: ' + points);
+ }
+ let question3 = prompt('Did the author attend TSU?', 'enter yes or no');
+    if (question3.toLowerCase() === 'no') {
+  alert('That is correct');
+  points++;
+  alert('Total points: ' + points)
+ } else {
+   alert('Incorrect');
+   points--;
+   alert('Total points: ' + points);
+ }
+ let question4 = prompt('Is the author a man?', 'enter yes or no');
+    if (question4.toLowerCase() === 'no') {
+  alert('That is correct');
+  points++;
+  alert('Total points: ' + points)
+} else {
+    alert('Incorrect');
+    points--;
+    alert('Total points: ' + points);
+}
+let question5 = prompt('Is the author a home owner?', 'enter yes or no');
+  if (question5.toLowerCase() === 'yes') {
+  alert('That is correct');
+  points++;
+  alert('Total points: ' + points)
+} else {
+    alert('Incorrect');
+    points--;
+    alert('Total points: ' + points);
+}
 }
 
-while (!user) {
-  alert ('Seriously, I need your name.');
-  user = prompt('What is your name?');
-}
 
 // convert the variable question1 to lower case to evaluate it
 // switch (question1.toLowerCase()) {
