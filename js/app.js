0970e5433443867fb3016f5d36f089d49bdec6e4
rtruneh <rosa.truneh@gmail.com>
2021-04-12 22:12:11 -0500

    fixed the function for question 6

--- js/app.js
+++ js/app.js
@@ -1,92 +1,90 @@
-// 'use strict'
-// //asking user if they want to play
-let answer1 = confirm('Would you like to play a game?')
-if (!answer1) {
-  alert('sorry to see you go');
-} else {
-  alert('come on in!');
-}
+// // 'use strict'
+// // //asking user if they want to play
+// let answer1 = confirm('Would you like to play a game?')
+// if (!answer1) {
+//   alert('sorry to see you go');
+// } else {
+//   alert('come on in!');
+// }
 
-//once user says ok, ask for name then enter the page
-let user = prompt('what is your name?');
-while (!user) {
-  alert ('Seriously, I need your name.');
-  user = prompt('What is your name?');
-} 
-if (user.length > 0) {
-confirm('Hi ' + user + ' let\'s play!');
-}
+// //once user says ok, ask for name then enter the page
+// let user = prompt('what is your name?');
+// while (!user) {
+//   alert ('Seriously, I need your name.');
+//   user = prompt('What is your name?');
+// } 
+// if (user.length > 0) {
+// confirm('Hi ' + user + ' let\'s play!');
+// }
 
 //game
-let points = 0
-function myQuiz() {
-  let question1 = prompt('Does the author have a child?', 'enter yes or no');
-    if (question1.toLowerCase() === 'yes') {
-  alert('That is correct');
-  points++;
-  alert('Total points: ' + points);
- } else {
-   alert('Incorrect');
-   alert('Total points: ' + points);
- }
- let question2 = prompt('Did the author work at the airport?', 'enter yes or no');
-    if (question2.toLowerCase() === 'yes') {
-  alert('That is correct');
-  points++;
-  alert('Total points: ' + points);
- } else {
-   alert('Incorrect');
-   alert('Total points: ' + points);
- }
- let question3 = prompt('Did the author attend TSU?', 'enter yes or no');
-    if (question3.toLowerCase() === 'no') {
-  alert('That is correct');
-  points++;
-  alert('Total points: ' + points)
- } else {
-   alert('Incorrect');
-   alert('Total points: ' + points);
- }
- let question4 = prompt('Is the author a man?', 'enter yes or no');
-    if (question4.toLowerCase() === 'no') {
-  alert('That is correct');
-  points++;
-  alert('Total points: ' + points)
-} else {
-    alert('Incorrect');
-    alert('Total points: ' + points);
-}
-let question5 = prompt('Is the author a home owner?', 'enter yes or no');
-  if (question5.toLowerCase() === 'yes') {
-  alert('That is correct');
-  points++;
-  alert('Total points: ' + points)
-} else {
-    alert('Incorrect');
-    alert('Total points: ' + points);
-}
-
+// let points = 0
 let i = 0;
-const answer = 10;
-let guess = prompt ('How many years did the author work at the airport?', 'enter number');
+let answer = 10;
 
-while (i < 4 && guess !== answer){
-  if(guess < answer){
-    alert('Not quite...try again');
-    guess = prompt('How many years did the author work at the airport?', 'enter number');
+
+function myQuiz() {
+
+//   let question1 = prompt('Does the author have a child?', 'enter yes or no');
+//     if (question1.toLowerCase() === 'yes') {
+//   alert('That is correct');
+//   points++;
+//   alert('Total points: ' + points);
+//  } else {
+//    alert('Incorrect');
+//    alert('Total points: ' + points);
+//  }
+//  let question2 = prompt('Did the author work at the airport?', 'enter yes or no');
+//     if (question2.toLowerCase() === 'yes') {
+//   alert('That is correct');
+//   points++;
+//   alert('Total points: ' + points);
+//  } else {
+//    alert('Incorrect');
+//    alert('Total points: ' + points);
+//  }
+//  let question3 = prompt('Did the author attend TSU?', 'enter yes or no');
+//     if (question3.toLowerCase() === 'no') {
+//   alert('That is correct');
+//   points++;
+//   alert('Total points: ' + points)
+//  } else {
+//    alert('Incorrect');
+//    alert('Total points: ' + points);
+//  }
+//  let question4 = prompt('Is the author a man?', 'enter yes or no');
+//     if (question4.toLowerCase() === 'no') {
+//   alert('That is correct');
+//   points++;
+//   alert('Total points: ' + points)
+// } else {
+//     alert('Incorrect');
+//     alert('Total points: ' + points);
+// }
+// let question5 = prompt('Is the author a home owner?', 'enter yes or no');
+//   if (question5.toLowerCase() === 'yes') {
+//   alert('That is correct');
+//   points++;
+//   alert('Total points: ' + points)
+// } else {
+//     alert('Incorrect');
+//     alert('Total points: ' + points);
+// }
+
+let question6 = prompt ('How many years did the author work at the airport?', 'enter number');
+while (i < 4 && question6 !== answer) { 
+  if (question6 < answer) {
+  alert(`Too low ${3 - i} attempts left`);
+  question6 = prompt('How many years did the author work at the airport?', 'enter number');
+    }
+  else if (question6 > answer) {
+    alert(`Too high ${3 - i} attempts left`);
+    question6 = prompt('How many years did the author work at the airport?', 'enter number');
+    }
+  else{
+    alert('That is correct!!');
   }
-  else if (!guess){
-    alert('please enter number');
-  }
-  else if (guess > answer){
-    alert('Not that long...try again');
-    guess = prompt('How many years did the author work at the airport?', 'enter number');
-  }
-  else if (guess === answer) {
-    alert('That is correct!!')
-    points++;
-  }
+  i++;
 }
-
+alert(`The answer is ${answer}`);
 }
-
